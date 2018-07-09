d2e2f6aa42584b28bd9286915e658ecd178a12c1
lichun <lichun@haitun.hk>
2018-07-09 21:44:51 +0800

    对userAddress增加一个策略

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\UserAddress;
+use App\Policies\UserAddressPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
