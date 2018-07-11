ca5379d2e1b9bf91ee7b35bea4e601aa2723a82d
lichun <lichun@haitun.hk>
2018-07-11 17:17:45 +0800

    用户界面 - 收藏商品列表

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -93,4 +93,12 @@ class ProductsController extends Controller
     }
 
 
+    public function favorites(Request $request)
+    {
+        $products = $request->user()->favoriteProducts()->paginate(16);
+
+        return view('products.favorites', ['products' => $products]);
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/wx/profile', 'WxController@profile')->name('wx.profile');
 
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
-Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+
 
 Auth::routes();
 
@@ -38,5 +38,8 @@ Route::group(['middleware' => 'auth'], function() {
         Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
         Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
         Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+        Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
+
     });
 });
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
