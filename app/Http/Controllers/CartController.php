b3cbc5be45efb51894b9ac6b641cc217a4f27175
lichun <lichun@haitun.hk>
2018-07-11 22:21:40 +0800

    商品加入购物车

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -13,8 +13,8 @@ class CartController extends Controller
 
     public function add(AddCartRequest $request)
     {
-        $user   = $request->user();
-        $skuId  = $request->input('sku_id');
+        $user = $request->user();
+        $skuId = $request->input('sku_id');
         $amount = $request->input('amount');
 
         // 从数据库中查询该商品是否已经在购物车中
@@ -32,6 +32,21 @@ class CartController extends Controller
             $cart->productSku()->associate($skuId);
             $cart->save();
         }
+
+        return [];
+    }
+
+
+    public function index(Request $request)
+    {
+        $cartItems = $request->user()->cartItems()->with(['productSku.product'])->get();
+        return view('cart.index', ['cartItems' => $cartItems]);
+    }
+
+
+    public function remove(ProductSku $sku, Request $request)
+    {
+        $request->user()->cartItems()->where('product_sku_id', $sku->id)->delete();
 
         return [];
     }
