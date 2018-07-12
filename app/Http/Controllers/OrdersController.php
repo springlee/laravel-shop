5dbffde241b232c60f58c96282edf087b1434733
lichun <lichun@haitun.hk>
2018-07-12 15:28:59 +0800

    关闭未支付订单

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\OrderRequest;
+use App\Jobs\CloseOrder;
 use App\Models\ProductSku;
 use App\Models\UserAddress;
 use App\Models\Order;
@@ -63,7 +64,7 @@ class OrdersController extends Controller
 
             return $order;
         });
-
+        $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
         return $order;
     }
 }
