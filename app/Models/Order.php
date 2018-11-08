434034df2a499cbf6fbae98f5568dac04e79f6a2
lichun <lichun@haitun.hk>
2018-11-08 19:39:00 +0800

    优惠券列表

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -87,6 +87,10 @@ class Order extends Model
     {
         return $this->hasMany(OrderItem::class);
     }
+    public function couponCode()
+    {
+        return $this->belongsTo(CouponCode::class);
+    }
 
     public static function findAvailableNo()
     {
